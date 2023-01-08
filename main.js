66d4e7280de92b13d9e509bd719488f45e408802
unknown <mateuscostaso1@gmail.com>
2023-01-08 01:35:00 -0300

    Desenvolvendo toda parte de CSS e dando inicio a parte de javascript

---
+++ main.js
@@ -0,0 +1,11 @@
+// string (texto)
+// Number (número)
+// boolean (true or false)
+const navigation = document.getElementById("navigation");
+function onScroll() {
+  if (scrollY > 0) {
+    navigation.classList.add("scroll");
+  } else {
+    navigation.classList.remove("scroll");
+  }
+}
