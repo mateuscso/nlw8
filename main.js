49897c8d1fee9ca578f4d136e600099d24154b9e
unknown <mateuscostaso1@gmail.com>
2023-01-11 14:55:16 -0300

    Finalizando aula 3

--- main.js
+++ main.js
@@ -9,3 +9,11 @@ function onScroll() {
     navigation.classList.remove("scroll");
   }
 }
+
+function openMenu() {
+  document.body.classList.add("menu-expanded");
+}
+
+function closeMenu() {
+  document.body.classList.remove("menu-expanded");
+}
