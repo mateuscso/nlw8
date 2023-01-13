4e3c08500d966a6c79e26a8e57e7d211e1447d7a
unknown <mateuscostaso1@gmail.com>
2023-01-13 03:30:50 -0300

    Finalizando a parte mobile aula 4

--- main.js
+++ main.js
@@ -1,12 +1,29 @@
 // string (texto)
 // Number (número)
 // boolean (true or false)
+window.addEventListener("scroll", onScroll);
+// onScroll();
+// executa o onscroll uma unica vez aqui, antes de tudo pra não dar erro e não faltar carregar nada.
+
 const navigation = document.getElementById("navigation");
 function onScroll() {
+  showNavOnScroll();
+  showBackToTopButtonOnScroll();
+}
+
+function showNavOnScroll() {
   if (scrollY > 0) {
     navigation.classList.add("scroll");
   } else {
     navigation.classList.remove("scroll");
+  }
+}
+
+function showBackToTopButtonOnScroll() {
+  if (scrollY > 550) {
+    backToTopButton.classList.add("show");
+  } else {
+    backToTopButton.classList.remove("show");
   }
 }
 
@@ -17,3 +34,18 @@ function openMenu() {
 function closeMenu() {
   document.body.classList.remove("menu-expanded");
 }
+
+ScrollReveal({
+  origin: "top",
+  distance: "30px",
+  duration: 700,
+}).reveal(`
+  #home, 
+  #home img, 
+  #home .stats, 
+  #services,
+  #services header,
+  #services .cards,
+  #about,
+  #about header,
+  #about .content`);
