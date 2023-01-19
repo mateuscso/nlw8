4f2597a6c3387c4c7b5e0f32bad6ff99a6441358
unknown <mateuscostaso1@gmail.com>
2023-01-19 09:58:28 -0300

    Finalização do projeto

--- main.js
+++ main.js
@@ -9,6 +9,48 @@ const navigation = document.getElementById("navigation");
 function onScroll() {
   showNavOnScroll();
   showBackToTopButtonOnScroll();
+
+  activateMenuAtCurrentSection(home);
+  activateMenuAtCurrentSection(services);
+  activateMenuAtCurrentSection(about);
+  activateMenuAtCurrentSection(contact);
+
+  // activateMenuAtCurrentSection(services);
+}
+
+function activateMenuAtCurrentSection(section) {
+  // linha alvo
+  const targetLine = scrollY + innerHeight / 2;
+  // linha alvo
+
+  // verificar se a seção passou da linha
+  // quais dados vou precisar?
+  const sectionTop = section.offsetTop;
+  const sectionHeight = section.offsetHeight;
+
+  // topo da seção chegou ou ultrapassou a linha alvo
+  const sectionTopReachOrPassedTargetLine = targetLine >= sectionTop;
+
+  // Verificar se a base está abaixo da linha alvo
+  // quais dados vou precisar?
+
+  // a sessão termina onde?
+  const sectionEndsAt = sectionTop + sectionHeight;
+
+  //o final da seção passou da linha alvo
+  const sectionEndPassedTargetline = sectionEndsAt <= targetLine;
+
+  // limites da sessão
+  const sectionBoundaries =
+    sectionTopReachOrPassedTargetLine && !sectionEndPassedTargetline;
+
+  const sectionId = section.getAttribute("id");
+  const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`);
+
+  menuElement.classList.remove("active");
+  if (sectionBoundaries) {
+    menuElement.classList.add("active");
+  }
 }
 
 function showNavOnScroll() {
